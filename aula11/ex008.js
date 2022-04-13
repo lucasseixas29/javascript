2b277c7c4a3ea2a3d9d0c7e0d9ffa7bc774e195b
Lucas Seixas <lucasseixas29@hotmail.com>
2022-04-13 00:05:37 -0300

    finalizando aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} por KM/H`)
+if(vel > 60) {
+        console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de segurança')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`vivendo no ${país}`)
+if(país == 'Brasil') {
+    console.log('Você é Brasileiro')
+} else{
+    console.log('Você é estrangeiro!')
+}
